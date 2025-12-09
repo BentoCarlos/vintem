d8d0f7d3d2d7d1ecc947da716438c4c8bb103ec2
Bento <bc.santos@pm.me>
2025-12-08 22:07:16 -0300

    Initial Commit

---
+++ meudindin/meudindinApp.swift
@@ -0,0 +1,17 @@
+//
+//  meudindinApp.swift
+//  meudindin
+//
+//  Created by Bento Carlos on 08/12/25.
+//
+
+import SwiftUI
+
+@main
+struct meudindinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
